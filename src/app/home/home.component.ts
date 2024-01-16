fa21b1582cc504e246b1a4673a8fd0afd3f0affe
02Ian <mohitathani15598@gmail.com>
2024-01-16 22:00:08 +0530

    Update UI and add user service (generated by blackbox.ai )

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,16 +24,16 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.postForm = this.fb.group({
-      imageUpload: ['',[Validators.required]],
-      firstname: ['',[Validators.required,Validators.maxLength(20)]],
-      lastname: ['',[Validators.required]],
-      email: ['',[Validators.required]],
-      phoneno: ['',[Validators.required]],
-      age: ['',[Validators.required]],
-      state: ['',[Validators.required]],
-      country: ['',[Validators.required]],
-      address: ['',[Validators.required]],
-      tags: ['',[Validators.required]]
+      imageUpload: this.fb.control(''),
+      firstname: this.fb.control(''),
+      lastname: this.fb.control(''),
+      email: this.fb.control(''),
+      phoneno: this.fb.control(''),
+      age: this.fb.control(''),
+      state: this.fb.control(''),
+      country: this.fb.control(''),
+      address: this.fb.control(''),
+      tags: this.fb.control('')
     });
 
     this.userService.getUsers().subscribe((res: User[]) => {
@@ -46,19 +46,19 @@ export class HomeComponent implements OnInit {
     this.fileInput.nativeElement.value = '';
   }
 
-  onSubmit(): void {
-    const user: User = {
-      firstname: this.Firstname.value,
-      lastname: this.Lastname.value,
-      email: this.Email.value,
-      phonenumber: this.PhoneNo.value,
-      age: this.Age.value,
-      state: this.State.value,
-      address: this.Address.value,
-      tags: this.Tags.value,
+  onSubmit() {
+    let user: User = {
+      firstname: this.postForm.get('firstname')?.value,
+      lastname: this.postForm.get('lastname')?.value,
+      email: this.postForm.get('email')?.value,
+      phonenumber: this.postForm.get('phoneno')?.value,
+      age: this.postForm.get('age')?.value,
+      state: this.postForm.get('state')?.value,
+      address: this.postForm.get('address')?.value,
+      tags: this.postForm.get('tags')?.value,
       photo: this.fileInput.nativeElement.files[0]?.name,
-      country: this.Country.value,
-      newsletter: this.NewSletter.value
+      country: this.postForm.get('country')?.value,
+      newsletter: this.postForm.get('newsletter')?.value
     };
 
     this.userService.postUsers(user).subscribe((res) => {
@@ -71,41 +71,41 @@ export class HomeComponent implements OnInit {
     return this.postForm.controls;
   }
 
-  public get Photo(): FormControl{
+  public get Photo(): FormControl {
     return this.postForm.get('photo') as FormControl;
   }
-  
-  public get Firstname(): FormControl{
+
+  public get Firstname(): FormControl {
     return this.postForm.get('firstname') as FormControl;
   }
-  
-  public get Lastname(): FormControl{
+
+  public get Lastname(): FormControl {
     return this.postForm.get('lastname') as FormControl;
   }
-  public get Email(): FormControl{
+  public get Email(): FormControl {
     return this.postForm.get('email') as FormControl;
   }
-  
-  public get PhoneNo(): FormControl{
+
+  public get PhoneNo(): FormControl {
     return this.postForm.get('phonenumber') as FormControl;
   }
-  public get Age(): FormControl{
+  public get Age(): FormControl {
     return this.postForm.get('age') as FormControl;
   }
-  public get State(): FormControl{
+  public get State(): FormControl {
     return this.postForm.get('state') as FormControl;
   }
-  public get Country(): FormControl{
+  public get Country(): FormControl {
     return this.postForm.get('country') as FormControl;
   }
-  public get Address(): FormControl{
+  public get Address(): FormControl {
     return this.postForm.get('address') as FormControl;
   }
-  public get Tags(): FormControl{
+  public get Tags(): FormControl {
     return this.postForm.get('tags') as FormControl;
   }
-  public get NewSletter(): FormControl{
+  public get NewSletter(): FormControl {
     return this.postForm.get('newsletter') as FormControl;
   }
-  
+
 }
--- src/app/user.mode.ts
+++ src/app/user.mode.ts
@@ -1,14 +1,14 @@
 export class User {
-    id?:number=0;
-    photo:string= '';
-firstname:string='';
-lastname:string='';
-email:string='';
-phonenumber:number=0;
-age:number=0;
-state:string='';
-country:string='';
-address:string='';
-tags:string=''
-newsletter:string=''
+    id?: number = 0;
+    photo: string = '';
+    firstname: string = '';
+    lastname: string = '';
+    email: string = '';
+    phonenumber: number = 0;
+    age: number = 0;
+    state: string = '';
+    country: string = '';
+    address: string = '';
+    tags: string = ''
+    newsletter: string = ''
 }
